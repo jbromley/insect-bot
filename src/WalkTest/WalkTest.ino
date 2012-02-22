5f387b7debd4989b347aa0373b9d985801723fe8
J. Bromley <jbromley@gmail.com>
2012-02-22 01:22:30 -0800

    Modified gait code.
    Define gaits as offsets from zero. Reduce all movement to a move
    function with a parameter that tells the kind of motion.

--- src/WalkTest/WalkTest.ino
+++ src/WalkTest/WalkTest.ino
@@ -8,40 +8,50 @@
  */
 #include <Servo.h>
 
+// The number of steps in a gait. There are four steps: movement 1, 
+// center, movement 2, and center. Each step has to move both the 
+// front and rear servos.
+const int GAIT_STEPS = 8;
+
+// Types of movements. These index into the gaits array.
 enum WalkType {
-  FORWARD, BACKWARD, LEFT, RIGHT, BACK_LEFT, BACK_RIGHT
+  FORWARD, BACKWARD, LEFT, RIGHT, BACK_LEFT, BACK_RIGHT, NUM_GAITS
 };
 
-enum {
-  STRIDE_DELAY = 5
-};
-
-const char frontCenter = 86;
-const char rearCenter = 84;
-
-char gaits[][8] = {
+// The gait positions.
+char gaits[][GAIT_STEPS] = {
   { -18, 15, 0, 0, 18, -15, 0, 0 },
   { -18, -15, 0, 0, 18, 15, 0, 0 },
   { -27, 6, -9, -9, 27, -24, -9, -9 },
-  { 27, -6, 9, 9, -27, 24, 9, 9},
-  { -18, -21, 0, -6, 27, 9, 0, 0},
-  { 18, 21, 0, 6, -27, -9, 0, 0}
+  { 27, -6, 9, 9, -27, 24, 9, 9 },
+  { -18, -21, 0, -6, 27, 9, 0, 0 },
+  { 18, 21, 0, 6, -27, -9, 0, 0 }
 };
 
-int delays[] = {125, 65, 125, 65, 65};
+// Delays between each of the four gait steps and the delay between
+// each gait cycle.
+int delays[] = { 125, 65, 125, 65 };
 
+// The delay between each gait cycle.
+const int STRIDE_DELAY = 65;
+
+// Center positions
+const char FRONT_CENTER = 86;
+const char REAR_CENTER = 84;
+
+// Servo controllers
 Servo frontServo;
 Servo rearServo;
 
 void setup()
 {
   // Set up the gait table.
-  for (int i = 0; i < 6; ++i) {
-    for (int j = 0; j < 8; ++j) {
+  for (int i = 0; i < NUM_GAITS; ++i) {
+    for (int j = 0; j < GAIT_STEPS; ++j) {
       if (i % 2 == 0) {
-	gaits[i][j] = gaits[i][j] + frontCenter;
+	gaits[i][j] = gaits[i][j] + FRONT_CENTER;
       } else {
-	gaits[i][j] = gaits[i][j] + rearCenter;
+	gaits[i][j] = gaits[i][j] + REAR_CENTER;
       }
     }
   }
@@ -52,26 +62,16 @@ void setup()
 
 void move(WalkType walk)
 {
+  const int walkIndex = int(walk);
   for (int i = 0; i < 4; ++i) {
-    frontServo.write(gaits[int(walk)][2 * i]);
-    rearServo.write(gaits[int(walk)][2 * i + 1]);
+    frontServo.write(gaits[walkIndex][2 * i]);
+    rearServo.write(gaits[walkIndex][2 * i + 1]);
     delay(delays[i]);
   }
 }
 
 void loop()
 {
-  // for (int i = 0; i < 10; ++i) {
-  moveForward();
-  delay(65);
-  // }
-  // for (int i = 0; i < 10; ++i) {
-  //   moveTurnLeft();
-  // }
-  // for (int i = 0; i < 10; ++i) {
-  //   moveBackward();
-  // }
-  // for (int i = 0; i < 10; ++i) {
-  //   moveBackRight();
-  // }
+  move(FORWARD);
+  delay(STRIDE_DELAY);
 }
