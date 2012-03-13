d3a0721ee847e6b2a4bff265714b1f52f11caa5d
J. Bromley <jbromley@gmail.com>
2012-03-13 01:29:40 -0700

    Added ultrasonic sensor and tilt switch.
    Added ultrasonic sensor to measure distance and two distance
    indicator LEDs. Added a tilt switch with tilt LED indicator.

--- src/LightTest/LightTest.ino
+++ src/LightTest/LightTest.ino
@@ -6,6 +6,12 @@
  * 4 (RS) -> D12
  * 5 (RW) -> D11
  * 6 (ENABLE) -> D10
+ * Ultrasonic trigger -> D6
+ * Ultrasonic echo -> D7
+ * tilt switch -> D8
+ * tilt indicator LED -> D9
+ * red distance LED -> A2
+ * yellow distance LED -> A3
  * 11 (D4) -> D5
  * 12 (D5) -> D4
  * 13 (D6) -> D3
@@ -19,16 +25,59 @@
  */
 #include <LiquidCrystal.h>
 
+// LCD 
 LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
 const int backLight = 13;
+
+// Photoresistors
 const int leftSensor = A0;
 const int rightSensor = A1;
+
+// Ultrasonic sensor
+const int triggerPin = 6;
+const int echoPin = 7;
+
+// Tilt switch and indicator
+const int tiltSwitchPin = 8;
+const int tiltLED = 9;
+
+// Distance indicator LEDs
+const int dangerLED = A2;
+const int warningLED = A3;
+
+void triggerUltrasonic()
+{
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+}
+
+long readDistanceCm()
+{
+  long echoTime = pulseIn(echoPin, HIGH);
+  return echoTime < 14500 ? echoTime / 59 : -1;
+}
 
 void setup()
 {
   pinMode(backLight, OUTPUT);
   pinMode(leftSensor, INPUT);
   pinMode(rightSensor, INPUT);
+
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  pinMode(tiltSwitchPin, INPUT);
+  digitalWrite(tiltSwitchPin, HIGH);
+  pinMode(tiltLED, OUTPUT);
+  digitalWrite(tiltLED, LOW);
+
+  pinMode(dangerLED, OUTPUT);
+  digitalWrite(dangerLED, LOW);
+  pinMode(warningLED, OUTPUT);
+  digitalWrite(warningLED, LOW);
 
   // Turn on the backlight.
   digitalWrite(backLight, HIGH);
@@ -42,6 +91,8 @@ void setup()
   lcd.print("R:");
   lcd.setCursor(0, 1);
   lcd.print("Light:");
+  lcd.setCursor(0, 2);
+  lcd.print("Distance (cm):");
 }
 
 String padWithSpaces(const String& s, int len)
@@ -54,7 +105,7 @@ String padWithSpaces(const String& s, int len)
   return result;
 }
 
-void display(int left, int right)
+void display(int left, int right, long distance)
 {
   String leftStr(left);
   String rightStr(right);
@@ -79,12 +130,42 @@ void display(int left, int right)
   
   lcd.setCursor(7, 1);
   lcd.print(lightDirection);
+
+  String distanceStr;
+  if (distance > 0) {
+    distanceStr = String(distance);
+  } else {
+    distanceStr = "----";
+  }
+  distanceStr = padWithSpaces(distanceStr, 4);
+  lcd.setCursor(15, 2);
+  lcd.print(distanceStr);
 }
 
 void loop()
 {
   int leftLight = analogRead(A1);
   int rightLight = analogRead(A0);
-  display(leftLight, rightLight);
+
+  triggerUltrasonic();
+  long distance = readDistanceCm();
+
+  if (0 < distance && distance < 10) {
+    digitalWrite(dangerLED, HIGH);
+    digitalWrite(warningLED, LOW);
+  } else if (10 < distance && distance  < 20) {
+    digitalWrite(dangerLED, LOW);
+    digitalWrite(warningLED, HIGH);
+  } else {
+    digitalWrite(dangerLED, LOW);
+    digitalWrite(warningLED, LOW);
+  }
+
+    
+
+  int tilt = digitalRead(tiltSwitchPin);
+  digitalWrite(tiltLED, tilt);
+
+  display(leftLight, rightLight, distance);
   delay(250);
 }
